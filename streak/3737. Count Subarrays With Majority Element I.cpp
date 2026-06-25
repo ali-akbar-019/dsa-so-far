2085dd796c2151d1ea01066d963a95511abdabfd
Ali Akbar <gamesforever018@gmail.com>
2026-06-25 11:34:49 -0700

    feat(streak): Implement Majority Element I and II solutions
    - Adds `Count Subarrays With Majority Element I.cpp`
    - Adds `Count Subarrays With Majority Element II.cpp`
    - Both solutions address counting subarrays where a target element is the majority.

---
+++ streak/3737. Count Subarrays With Majority Element I.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    int countMajoritySubarrays(vector<int> &nums, int target)
+    {
+
+        int n = nums.size();
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int sum = 0;
+            for (int j = i; j < n; j++)
+            {
+                if (nums[j] == target)
+                {
+                    sum++;
+                }
+                else
+                {
+                    sum--;
+                }
+                if (sum > 0)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
