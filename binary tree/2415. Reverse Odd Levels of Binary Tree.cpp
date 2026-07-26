7620205b5faf4ff904679200288c0cfee9b658a8
Ali Akbar <gamesforever018@gmail.com>
2026-07-26 14:43:27 -0700

    feat(binary_tree): Implement reverse odd levels solution
    - Adds a new solution for reversing values at odd levels of a binary tree.
    - Utilizes a recursive helper function `solve` to traverse and swap values.
    - Handles base cases and swaps values at odd levels (level 1, 3, etc.).

---
+++ binary tree/2415. Reverse Odd Levels of Binary Tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    void solve(TreeNode *L, TreeNode *R, int level)
+    {
+        if (L == NULL || R == NULL)
+            return;
+        if (level % 2 == 1)
+        {
+            //
+            swap(L->val, R->val);
+        }
+        solve(L->left, R->right, level + 1);
+        solve(L->right, R->left, level + 1);
+    }
+    TreeNode *reverseOddLevels(TreeNode *root)
+    {
+        solve(root->left, root->right, 1);
+        return root;
+    }
+};
