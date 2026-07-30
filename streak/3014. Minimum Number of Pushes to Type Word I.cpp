03a249256dc277e6903ef46a5f101605e870f72e
Ali Akbar <gamesforever018@gmail.com>
2026-07-30 12:50:39 -0700

    POTD 07-30-2026

---
+++ streak/3014. Minimum Number of Pushes to Type Word I.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int minimumPushes(string word)
+    {
+        unordered_map<int, int> mp;
+        int assign_key = 2;
+        int res = 0;
+        for (auto &w : word)
+        {
+            if (assign_key > 9)
+                assign_key = 2;
+            mp[assign_key]++;
+            res += mp[assign_key];
+            assign_key++;
+        }
+        return res;
+    }
+};
