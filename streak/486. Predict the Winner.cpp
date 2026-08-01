332c4850d497391ce7435450cefc5d93ebfa0903
Ali Akbar <gamesforever018@gmail.com>
2026-08-01 11:39:37 -0700

    POTD 08-01-2026

---
+++ streak/486. Predict the Winner.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    int t[21][21];
+    int solve(int i, int j, vector<int> &nums)
+    {
+        if (i > j)
+            return 0;
+        if (i == j)
+            return nums[i];
+        if (t[i][j] != -1)
+            return t[i][j];
+        int take_i = nums[i] + min(solve(i + 2, j, nums), solve(i + 1, j - 1, nums));
+        int take_j = nums[j] + min(solve(i + 1, j - 1, nums), solve(i, j - 2, nums));
+        return t[i][j] = max(take_i, take_j);
+    }
+    bool predictTheWinner(vector<int> &nums)
+    {
+        // total sum
+        int n = nums.size();
+        memset(t, -1, sizeof(t));
+        int sum = accumulate(begin(nums), end(nums), 0);
+        int P1 = solve(0, n - 1, nums);
+        int P2 = sum - P1;
+        return P1 >= P2;
+    }
+};
