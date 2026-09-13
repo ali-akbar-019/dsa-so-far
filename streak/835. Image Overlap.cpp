3553f5cb00504a02d982aa66ae4f88ebb6b41aa8
Ali Akbar <gamesforever018@gmail.com>
2026-09-13 13:22:08 -0700

    POTD 09-13-2026

---
+++ streak/835. Image Overlap.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+public:
+    int n;
+
+    int calcOverlapCount(vector<vector<int>> &A, vector<vector<int>> &B, int rowOff, int colOff)
+    {
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                int B_i = i + rowOff;
+                int B_j = j + colOff;
+                if (B_i < 0 || B_i >= n || B_j < 0 || B_j >= n)
+                {
+                    continue;
+                }
+                if (A[i][j] == 1 && B[B_i][B_j] == 1)
+                {
+                    count += 1;
+                }
+            }
+        }
+        return count;
+    }
+    int largestOverlap(vector<vector<int>> &A, vector<vector<int>> &B)
+    {
+        // offsets check krne ha
+        n = A.size();
+
+        int maxCount = 0;
+        for (int rowOff = -n + 1; rowOff < n; rowOff++)
+        {
+            for (int colOff = -n + 1; colOff < n; colOff++)
+            {
+                int count = calcOverlapCount(A, B, rowOff, colOff);
+                maxCount = max(count, maxCount);
+            }
+        }
+        return maxCount;
+    }
+};
